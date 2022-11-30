f1fe105a5105ecf6e52cda6ddf2f0132d6e85cbf
Martin-Melody <78037342+Martin-Melody@users.noreply.github.com>
2022-11-30 18:07:01 +0000

    Buttons png added for both states, game screen added and button to from home screen to button screen, orientation set to landscape.

---
+++ app/src/main/java/com/example/madfinalproject/Game_Screen.java
@@ -0,0 +1,17 @@
+package com.example.madfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Game_Screen extends AppCompatActivity{
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_screen);
+    }
+
+}
+
+
--- app/src/main/java/com/example/madfinalproject/MainActivity.java
+++ app/src/main/java/com/example/madfinalproject/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.madfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnPlayGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
+
+        btnPlayGame = findViewById(R.id.btnPlayGame);
+        btnPlayGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Game_Screen.class));
+            }
+        });
+
     }
+
 }
+
